bad6edaa752e0d92befb3fd6344c72fd7383860b
Xtr126 <crawleymsh@gmail.com>
2023-10-18 17:35:17 +0530

    Add translated text #58

---
+++ src/generated/locales/ar.js
@@ -0,0 +1,26 @@
+
+    // Do not modify this file by hand!
+    // Re-generate this file by running lit-localize
+
+    
+    
+
+    /* eslint-disable no-irregular-whitespace */
+    /* eslint-disable @typescript-eslint/no-explicit-any */
+
+    export const templates = {
+      's65aa9c025267e5cd': `translated text`,
+'sae1c7f13b7c0c47f': `Start`,
+'s78e51a17af6607c3': `Create /data directory`,
+'s877c918c3f399cba': `Create data.img`,
+'s26bf81a54b3c98fd': `Install for QEMU`,
+'sc16e00a7a8b2fde2': `Back`,
+'sff3418011f0f7a2f': `Extracting ISO`,
+'s4396b8c2cb0d7d78': `Open /etc/grub.d/40_custom or /boot/grub/grub.cfg in a text editor`,
+'sc27dc998602cdc61': `Example:`,
+'sb746ace08dc971f5': `Create a new grub entry with the following code: `,
+'sbc8741ff143d32f6': ` After editing /etc/grub.d/40_custom, re-generate grub config with command:`,
+'s701c80c5e6ceabf2': `The exact command to use may vary depending on your distro.`,
+'s0379fc73608ab971': `Done`,
+    };
+  
