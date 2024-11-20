4c7b4e747f5a50fc4a864437bec9056d84a3dfc8
Xtr126 <k.gunetilleke@gmail.com>
2024-11-20 16:46:48 +0530

    Check for administrative privileges

--- src-tauri/src/windows.rs
+++ src-tauri/src/windows.rs
@@ -3,18 +3,20 @@ use std::{
     process::{Command, Output},
 };
 
+use std::io::{self, Write};
+
 // Helper function to execute a command and capture its output
 pub fn run_command(description: &str, command: &str) -> Output {
-    println!("{}", description);
-    println!("{}", command);
+    println!(description);
+    println!(command);
 
     let output = Command::new("cmd")
         .args(&["/C", command])
         .output()
         .expect("Failed to execute command");
 
-    eprintln!("{}", String::from_utf8_lossy(&output.stderr));
-    println!("{}", String::from_utf8_lossy(&output.stdout));
+    io::stdout().write_all(&output.stdout).unwrap();
+    io::stderr().write_all(&output.stderr).unwrap();
 
     output
 }
@@ -25,10 +27,18 @@ pub fn mount_efi_system_partition(esp_drive_letter: &str) {
         &format!("mountvol {esp_drive_letter} /d"),
     );
 
-    run_command(
-        &format!("=== Step 2: Mounting EFI System Partition ==="),
-        &format!("mountvol {esp_drive_letter} /s"),
-    );
+    println!("=== Step 2: Mounting EFI System Partition ===");
+    println!("mountvol {esp_drive_letter} /s");
+
+    let status = Command::new("cmd")
+        .args(&["/C", command])
+        .status()
+        .expect("Failed to execute command");
+
+    if ! status.success() {
+       eprintln!("Failed to mount EFI system partition");
+       crate::cli::ask_to_exit();
+    } 
 }
 
 pub fn unmount_efi_system_partition(esp_drive_letter: &str) {
