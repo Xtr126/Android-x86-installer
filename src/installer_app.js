46619a7e7c3f6b28ec3e49f514ebe1cd6d73b1b8
Xtr126 <80520774+Xtr126@users.noreply.github.com>
2024-11-24 15:39:27 +0530

    Update bootloader-windows section

--- src/installer_app.js
+++ src/installer_app.js
@@ -133,7 +133,7 @@ export class InstallerApp extends LitElement {
       background-color: var(--md-sys-color-surface-container-high);
       border-radius: 1em;
       border-width: 1px;
-      margin-bottom: 1.5rem;
+      margin: 1rem;
       overflow-x: auto;
       color: var(--md-sys-color-on-surface-variant);
       text-shadow: 0 1px 1px var(--md-sys-color-surface);
@@ -141,7 +141,6 @@ export class InstallerApp extends LitElement {
       font-size: 14px;
       tab-size: 2;
       line-height: 1.5;
-      --md-elevation-level: 5;
     }
 
     .copy-button {
@@ -257,21 +256,20 @@ export class InstallerApp extends LitElement {
         <li>${msg('Open /etc/grub.d/40_custom or /boot/grub/grub.cfg in a text editor')}</li><br>
         <div>${msg('Example:')}</div>
         <div class="codeblock-surface" > 
-        <pre><code>  sudo nano /etc/grub.d/40_custom</code></pre>
+          <pre><code>  sudo nano /etc/grub.d/40_custom</code></pre>
         </div>
-        <li>${msg('Create a new grub entry with the following code: ')}</li>
+        <li>${msg('Create a new grub entry with the following code:')}</li><br>
         <div class="codeblock-surface">
-        <md-elevation></md-elevation>
-        <div style="position: relative" title="Copy">
-        <md-icon-button class="copy-button" @click="${this.copyCode}">
-          <md-icon>
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="16" viewBox="-3 -4 30 30" role="presentation"><g fill="currentColor"><g><path d="M20 8h-9c-1.65 0-3 1.35-3 3v9c0 1.65 1.35 3 3 3h9c1.65 0 3-1.35 3-3v-9c0-1.65-1.35-3-3-3zm1 12c0 .55-.45 1-1 1h-9c-.55 0-1-.45-1-1v-9c0-.55.45-1 1-1h9c.55 0 1 .45 1 1v9z"></path><path d="M5 14H4c-.55 0-1-.45-1-1V4c0-.55.45-1 1-1h9c.55 0 1 .45 1 1v1c0 .55.45 1 1 1s1-.45 1-1V4c0-1.65-1.35-3-3-3H4C2.35 1 1 2.35 1 4v9c0 1.65 1.35 3 3 3h1c.55 0 1-.45 1-1s-.45-1-1-1z"></path></g></g></svg>
-          </md-icon>
-        </md-icon-button>
+          <div style="position: relative" title="Copy">
+            <md-icon-button class="copy-button" @click="${this.copyCode}">
+              <md-icon>
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="16" viewBox="-3 -4 30 30" role="presentation"><g fill="currentColor"><g><path d="M20 8h-9c-1.65 0-3 1.35-3 3v9c0 1.65 1.35 3 3 3h9c1.65 0 3-1.35 3-3v-9c0-1.65-1.35-3-3-3zm1 12c0 .55-.45 1-1 1h-9c-.55 0-1-.45-1-1v-9c0-.55.45-1 1-1h9c.55 0 1 .45 1 1v9z"></path><path d="M5 14H4c-.55 0-1-.45-1-1V4c0-.55.45-1 1-1h9c.55 0 1 .45 1 1v1c0 .55.45 1 1 1s1-.45 1-1V4c0-1.65-1.35-3-3-3H4C2.35 1 1 2.35 1 4v9c0 1.65 1.35 3 3 3h1c.55 0 1-.45 1-1s-.45-1-1-1z"></path></g></g></svg>
+              </md-icon>
+            </md-icon-button>
+          </div>
+          <pre><code>${this.bootloaderMsg_}</code></pre>
         </div>
-        <pre><code>${this.bootloaderMsg_}</code></pre>
-        </div>
-        <li>${msg(' After editing /etc/grub.d/40_custom, re-generate grub config with command:')}</li>
+        <li>${msg('After editing /etc/grub.d/40_custom, re-generate grub config with command:')}</li><br>
         <div class="codeblock-surface" > 
         <pre><code>  sudo grub-mkconfig -o /boot/grub/grub.cfg</code></pre>
         </div>
@@ -282,26 +280,31 @@ export class InstallerApp extends LitElement {
 
     <section class="installer-app-category" ?active-category="${this.activeCategory_ === 'bootloader-windows'}">
       <div class="display-small" style="margin-left: 50px;">
-        <div>${msg('Only Bliss OS Grub can read from NTFS partitions.')}</div>
-        <div>${msg('Compression should be disabled in properties for the partition.')}</div><br>
-
-        <div>${msg('Press Win+X and select Windows Terminal (Admin)')}</div>
-        <div>${msg('Open PowerShell by typing in below command and press enter key.')}</div>
+        <h4>${msg('Important')}</h4>
+        <li>${msg('Only Bliss OS Grub can read from NTFS partitions.')}</li>
+        <li>${msg('Compression should be disabled in drive properties for all files.')}</li>
+        <li>${msg('Fastboot/hibernation should be disabled.')}</li>
+        <br>
+          
+        <h4>${msg('Step 1 - Open PowerShell')}</h4>
+        <li>${msg('Press Win+X and select Windows Terminal (Admin) or Powershell')}</li>
+        <li>${msg('Type below command and press enter key.')}</li>
         <div class="codeblock-surface" > 
           <pre><code>  powershell.exe</code></pre>
         </div>
-        <div>${msg('Right click to paste text into the terminal.')}</div><br>
-        <div>${msg('Mount EFI System partition')}</div>
+        <li>${msg('Tip: Right click to paste text into the terminal.')}</li><br>
+        <h4>${msg('Step 2 - Mount EFI System partition')}</h4>
           <div class="codeblock-surface" > 
+            <pre><code>  mountvol X: /d</code></pre>
             <pre><code>  mountvol X: /s</code></pre>
           </div>
-        <div>${msg('Copy Android bootloader files')}</div>
+        <h4>${msg('Step 3 - Copy Android bootloader files')}</h4>
           <div class="codeblock-surface" > 
             <pre><code>  Copy-Item -Path ${this.installDir}\\boot -Destination X:\\ -Recurse -Force -Confirm</code></pre>
             <pre><code>  Copy-Item -Path ${this.installDir}\\efi\\boot -Destination X:\\EFI\\ -Recurse -Force -Confirm</code></pre>
           </div>
 
-        <div>${msg('Create bootloader entry for Android')}</div>
+        <h4>${msg('Step 4 - Create bootloader entry for Android')}</h4>
 
         <div class="codeblock-surface" > 
           <div style="margin-left: 10px;">${msg('Windows 11:')}</div>
@@ -312,25 +315,25 @@ export class InstallerApp extends LitElement {
           <pre><code>  bcdedit /copy '{bootmgr}' /d "Android"</code></pre>
         </div>
 
-        <div>${msg('Use x-x-xxxx-xxxxx from identifier {x-x-xxxx-xxxxx} in output from previous command.')}</div>
+        <li>${msg('Use xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx from identifier {xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx} in output from previous command.')}</li>
 
         <div class="codeblock-surface" > 
           <div style="margin-left: 10px;">${msg('Windows 11:')}</div>
-          <pre><code>  Set-BcdElement -Element path -Id x-x-xxxx-xxxxx -Type String -Value \\EFI\\boot\\BOOTx64.EFI</code></pre>
+          <pre><code>  Set-BcdElement -Element path -Id xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx -Type String -Value \\EFI\\boot\\BOOTx64.EFI</code></pre>
         </div>
         <div class="codeblock-surface" > 
           <div style="margin-left: 10px;">${msg('Windows 10:')}</div>
-          <pre><code>  bcdedit /set '{x-x-xxxx-xxxxx}' path \\EFI\\boot\\BOOTx64.EFI</code></pre>
+          <pre><code>  bcdedit /set '{xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx}' path \\EFI\\boot\\BOOTx64.EFI</code></pre>
         </div>
 
-        <div>${msg('Disable hibernation using PowerShell to avoid bootloop.')}</div>
-        <div>${msg('If turning off hibernation is undesirable, hold down shift key when shutting down Windows to perform a full shutdown, everytime before booting to Android.')}</div>
+        <h4>${msg('Step 5 - Disable hibernation using PowerShell to avoid bootloop.')}</h4>
+        <li>${msg('If turning off hibernation is undesirable, hold down shift key when shutting down Windows to perform a full shutdown, everytime before booting to Android.')}</li>
         <div class="codeblock-surface" > 
           <pre><code>  powercfg.exe /hibernate off</code></pre>
         </div>
 
-        <div>${msg('Boot to Android from UEFI Boot Menu in BIOS.')}</div>
-        <div>${msg('Or press Ctrl+Alt+Del, click on power button and hold shift key while clicking on restart. After restarting, select "Use another device" or "Use another operating system" from the menu and select "Android".')}</div>
+        <div>${msg('Now you can boot to Android from UEFI Boot Menu in BIOS.')}</div>
+        <p>${msg('Or press Ctrl+Alt+Del, click on power button and hold shift key while clicking on restart. After restarting, select "Use another device" or "Use another operating system" from the menu and select "Android".')}</p>
         <pre>
           
         </pre>
