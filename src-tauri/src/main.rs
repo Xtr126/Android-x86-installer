51266254a4356e747949a6e1c11020b5cf1585f9
Xtr126 <80520774+Xtr126@users.noreply.github.com>
2024-11-27 11:32:22 +0530

    Disable console on release builds

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,3 +1,8 @@
+#![cfg_attr(
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
+)]
+
 use compress_tools::{uncompress_archive, Ownership};
 use error::Error;
 use progress::Progress;
